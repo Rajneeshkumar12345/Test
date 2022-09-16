c14091a820009c4c0c5da9318161bc242378d0d4
Rajneeshkumar12345 <rkv7861999@gmail.com>
2022-09-17 03:27:10 +0530

    Add

---
+++ Assignment_3/Main.java
@@ -0,0 +1,19 @@
+package Assignment_3;
+
+public class Main {
+    Scanner inputData = new Scanner(System.in);
+    System.out.println("Enter a Sentence(any English sentence)");
+    Stirng sentence = inputData.nextLine();
+    System.out.println("Enter a Letter(any letter from A to Z)");
+    String letter = inputData.nextLine();
+
+    int index = sentence.indexOf(letter);
+
+    if(index == -1){
+        System.out.println('The letter does not exist in the sentence');
+        
+    } else {
+        System.out.println(sentence.substring(index+1));
+    }
+
+}
